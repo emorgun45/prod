9564d698cf6d311d4779583851e61f30aedeccf6
emorgun45 <emirhandolgun@outlook.com>
2023-06-08 00:50:47 +0300

    added todo list

---
+++ js/buttons.js
@@ -0,0 +1,1 @@
+addBtn.onclick = addTodo
--- js/variables.js
+++ js/variables.js
@@ -2,6 +2,9 @@ let clockdiv = document.querySelector(".clock");
 // let range = document.querySelector(".range");
 let start = document.querySelector(".start");
 let reset = document.querySelector(".stop");
+let addBtn = document.querySelector(".add-btn")
+let todoList = document.querySelector(".todo-list > ul")
+let addInput = document.querySelector(".add-input")
 // let rangeValue = document.querySelector(".range").value
 
 const d = new Date();
